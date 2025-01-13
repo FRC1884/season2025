2737e59c8e3a1857ae675697804e0bf74cd9b62b
EXIBIST32132 <81558382+EXIBIST32132@users.noreply.github.com>
2025-01-13 16:58:34 +0000

    Teaching subsystems

--- src/main/java/frc/robot/subsystems/Superstructure.java
+++ src/main/java/frc/robot/subsystems/Superstructure.java
@@ -9,25 +9,12 @@ import edu.wpi.first.wpilibj.util.Color;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import edu.wpi.first.wpilibj2.command.button.Trigger;
-import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
 import frc.robot.Config;
 import frc.robot.GlobalConstants;
-import frc.robot.subsystems.climber.ClimberIOReal;
-import frc.robot.subsystems.climber.ClimberIOSim;
-import frc.robot.subsystems.climber.ClimberSubsystem;
-import frc.robot.subsystems.elevator.ElevatorIOReal;
-import frc.robot.subsystems.elevator.ElevatorIOSim;
-import frc.robot.subsystems.elevator.ElevatorSubsystem;
-import frc.robot.subsystems.feeder.FeederIOReal;
-import frc.robot.subsystems.feeder.FeederIOSim;
-import frc.robot.subsystems.feeder.FeederSubsystem;
+
 import frc.robot.subsystems.leds.LEDIOPWM;
 import frc.robot.subsystems.leds.LEDIOSim;
 import frc.robot.subsystems.leds.LEDSubsystem;
-import frc.robot.subsystems.pivot.PivotIOReal;
-import frc.robot.subsystems.pivot.PivotIOSim;
-import frc.robot.subsystems.pivot.PivotSubsystem;
 import frc.robot.util.AllianceFlipUtil;
 import java.util.function.Supplier;
 import org.littletonrobotics.junction.networktables.LoggedDashboardChooser;
@@ -39,34 +26,6 @@ public class Superstructure extends SubsystemBase {
    * states, it's idling.
    */
   private Supplier<Pose2d> drivePoseSupplier;
-
-  private final PivotSubsystem pivot =
-      Config.Subsystems.ELEVATOR_ENABLED
-          ? (MODE == GlobalConstants.RobotMode.REAL
-              ? new PivotSubsystem("Pivot", new PivotIOReal())
-              : new PivotSubsystem("Pivot Sim", new PivotIOSim(2, 0.0)))
-          : null;
-  private final FeederSubsystem feeder =
-      Config.Subsystems.FEEDER_ENABLED
-          ? (MODE == GlobalConstants.RobotMode.REAL
-              ? new FeederSubsystem("Feeder", new FeederIOReal())
-              : new FeederSubsystem(
-                  "Feeder Sim", new FeederIOSim(new DCMotor(1, 1, 1, 1, 1, 1), 0, 0)))
-          : null;
-
-  private final ClimberSubsystem climber =
-      Config.Subsystems.ELEVATOR_ENABLED
-          ? (MODE == GlobalConstants.RobotMode.REAL
-              ? new ClimberSubsystem("Climber", new ClimberIOReal())
-              : new ClimberSubsystem("Climber Sim", new ClimberIOSim(2, 0.0)))
-          : null;
-
-  private final ElevatorSubsystem elevator =
-      Config.Subsystems.ELEVATOR_ENABLED
-          ? (MODE == GlobalConstants.RobotMode.REAL
-              ? new ElevatorSubsystem("Climber", new ElevatorIOReal())
-              : new ElevatorSubsystem("Climber Sim", new ElevatorIOSim(2, 0.0)))
-          : null;
 
   private final LEDSubsystem leds =
       Config.Subsystems.LEDS_ENABLED
@@ -104,11 +63,6 @@ public class Superstructure extends SubsystemBase {
   public void periodic() {
     switch (currentState) {
       case IDLING -> {
-        if (feeder != null) feeder.setGoal(FeederSubsystem.FeederGoal.IDLING);
-        // if (flywheel != null)
-        if (pivot != null) pivot.setGoal(PivotSubsystem.PivotGoal.IDLING);
-        if (climber != null) climber.setGoal(ClimberSubsystem.ClimberGoal.IDLING);
-        if (elevator != null) elevator.setGoal(ElevatorSubsystem.ElevatorGoal.IDLING);
         if (leds != null)
           leds.setRunAlongCmd(
               () -> AllianceFlipUtil.shouldFlip() ? Color.kRed : Color.kBlue,
--- src/main/java/frc/robot/subsystems/climber/ClimberIO.java
+++
@@ -1,5 +0,0 @@
-package frc.robot.subsystems.climber;
-
-import frc.robot.generic.elevators.GenericElevatorSystemIO;
-
-public interface ClimberIO extends GenericElevatorSystemIO {}
