43fa7f2b224c46a474a81075b4f1a26bb9fb574e
masterswordio <68434292+masterswordio@users.noreply.github.com>
2025-01-14 19:54:30 +0000

    ...

--- src/main/java/frc/robot/subsystems/pivot/PivotIO.java
+++ src/main/java/frc/robot/subsystems/pivot/PivotIO.java
@@ -1,4 +1,11 @@
 package frc.robot.subsystems.pivot;
 
 public interface PivotIO {
-  
+    public class PivotIOInputs {
+        public double degrees;
+    }
+
+    default void setDegrees(double degrees) {};
+
+    default void updateInputs(PivotIOInputs io) {};
+}
--- src/main/java/frc/robot/subsystems/pivot/PivotIOReal.java
+++ src/main/java/frc/robot/subsystems/pivot/PivotIOReal.java
@@ -1,21 +1,4 @@
 package frc.robot.subsystems.pivot;
 
 import com.revrobotics.RelativeEncoder;
-import com.revrobotics.spark.SparkLowLevel;
-import com.revrobotics.spark.SparkMax;
-import edu.wpi.first.math.controller.PIDController;
-
-public class PivotIOReal implements PivotIO{
-
-    private SparkMax motor = new SparkMax(PivotConstants.PIVOT_ID, SparkLowLevel.MotorType.kBrushless);
-    private PIDController pid = new PIDController(PivotConstants.kp, PivotConstants.ki, PivotConstants.kd);
-    private RelativeEncoder encoder = motor.getEncoder();
-
-    public void setDegrees(double degrees) {
-        motor.setVoltage(pid.calculate(encoder.getPosition(), degrees));
-    };
-
-    public void updateInputs(PivotIOInputs io) {
-        io.degrees = encoder.getPosition();
-    };
-}
+import 
