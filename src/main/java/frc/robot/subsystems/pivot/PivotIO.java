98d885b65592195956f7159ab1bac5b67d068511
masterswordio <68434292+masterswordio@users.noreply.github.com>
2025-01-14 19:49:11 +0000

    Created PivotGoal and ClimberGoal enums. Fixed typo in PivotIO.

--- src/main/java/frc/robot/subsystems/pivot/PivotIO.java
+++ src/main/java/frc/robot/subsystems/pivot/PivotIO.java
@@ -1,11 +1,4 @@
 package frc.robot.subsystems.pivot;
 
 public interface PivotIO {
-    public class PivotIOInputs {
-        public double degrees;
-    }
-
-    default void getDegrees(double degrees) {};
-
-    default void updateInputs(PivotIOInputs io) {};
-}
+  
--- src/main/java/frc/robot/subsystems/pivot/PivotSubsystem.java
+++ src/main/java/frc/robot/subsystems/pivot/PivotSubsystem.java
@@ -1,6 +1,26 @@
 package frc.robot.subsystems.pivot;
 
+import edu.wpi.first.units.measure.Distance;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import lombok.RequiredArgsConstructor;
+
+import java.util.function.DoubleSupplier;
 
 public class PivotSubsystem extends SubsystemBase {
+
+    @RequiredArgsConstructor
+    public enum PivotGoal {
+        IDLING(() -> 0.0),
+        LEVEL_ONE(() -> 0.1),
+        LEVEL_TWO(() -> 0.2),
+        LEVEL_THREE(() -> 0.3),
+        LEVEL_FOUR(() -> 0.4);
+
+        private final DoubleSupplier heightSupplier;
+
+        public double getHeightSupplier() {
+            return heightSupplier.getAsDouble();
+        }
+    }
+
 }
