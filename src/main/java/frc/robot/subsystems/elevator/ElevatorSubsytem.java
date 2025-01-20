c4ef8a9da8d0337ee6f2754049cd8266c010c658
adrian-fk <adrian_frazier_kim@asl.org>
2025-01-20 17:09:13 +0000

    The base yay.

--- src/main/java/frc/robot/subsystems/elevator/ElevatorSubsytem.java
+++ src/main/java/frc/robot/subsystems/elevator/ElevatorSubsytem.java
@@ -1,26 +1,34 @@
-//package frc.robot.subsystems.elevator;
-//
-//import edu.wpi.first.math.filter.Debouncer;
-//import edu.wpi.first.wpilibj2.command.SubsystemBase;
-//
-//
-//import java.util.function.DoubleSupplier;
-//
-//public class ElevatorSubsystem extends SubsystemBase {
-//    public enum ElevatorGoal {
-//        LEVELONE(()-> 0.1),
-//        LEVELTWO(()-> 0.2),
-//        LEVELTHREE(()-> 0.3),
-//        IDLE(()-> 0.0);
-//    }
-//    private final DoubleSupplier heightSupplier;
-//
-//    public double getHeightSupplier() {
-//        return heightSupplier.getAsDouble();
-//    }
-//}
-//
-//private ElevatorSubsystem.ElevatorGoal goal = ElevatorSubsystem.ElevatorGoal.IDLE;
-//private Debouncer currentDebouncer = new Debouncer(0.25, Debouncer.DebounceType.kFalling);
-//}
-//}
+package frc.robot.subsystems.elevator;
+
+import edu.wpi.first.math.filter.Debouncer;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import lombok.RequiredArgsConstructor;
+
+
+import java.util.function.DoubleSupplier;
+
+public class ElevatorSubsystem extends SubsystemBase {
+    public ElevatorSubsystem(DoubleSupplier heightSupplier) {
+        this.heightSupplier = heightSupplier;
+    }
+
+    @RequiredArgsConstructor
+    public enum ElevatorGoal {
+       // LEVELONE(()-> 0.1),
+       // LEVELTWO(()-> 0.2),
+        //LEVELTHREE(()-> 0.3),
+        //IDLE(()-> 0.0);
+    }
+    private final DoubleSupplier heightSupplier;
+
+    public double getHeightSupplier() {
+
+        return heightSupplier.getAsDouble();
+    }
+
+
+private ElevatorSubsystem.ElevatorGoal goal = ElevatorSubsystem.ElevatorGoal.IDLE;
+
+private Debouncer currentDebouncer = new Debouncer(0.25, Debouncer.DebounceType.kFalling);
+}
+
