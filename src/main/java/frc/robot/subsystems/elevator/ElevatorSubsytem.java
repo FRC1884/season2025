99d8b9eee8a253ad2d70716d5d683d7f5565d288
adrian-fk <adrian_frazier_kim@asl.org>
2025-01-20 17:05:19 +0000

    The base yay.

--- src/main/java/frc/robot/subsystems/elevator/ElevatorSubsytem.java
+++ src/main/java/frc/robot/subsystems/elevator/ElevatorSubsytem.java
@@ -1,4 +1,26 @@
-package frc.robot.subsystems.elevator;
-
-public class ElevatorSubsytem {
-}
+//package frc.robot.subsystems.elevator;
+//
+//import edu.wpi.first.math.filter.Debouncer;
+//import edu.wpi.first.wpilibj2.command.SubsystemBase;
+//
+//
+//import java.util.function.DoubleSupplier;
+//
+//public class ElevatorSubsystem extends SubsystemBase {
+//    public enum ElevatorGoal {
+//        LEVELONE(()-> 0.1),
+//        LEVELTWO(()-> 0.2),
+//        LEVELTHREE(()-> 0.3),
+//        IDLE(()-> 0.0);
+//    }
+//    private final DoubleSupplier heightSupplier;
+//
+//    public double getHeightSupplier() {
+//        return heightSupplier.getAsDouble();
+//    }
+//}
+//
+//private ElevatorSubsystem.ElevatorGoal goal = ElevatorSubsystem.ElevatorGoal.IDLE;
+//private Debouncer currentDebouncer = new Debouncer(0.25, Debouncer.DebounceType.kFalling);
+//}
+//}
