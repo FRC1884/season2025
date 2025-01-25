8d8cceb91084cbf9f1fd39a1fb4c468dd8786ae0
adrian-fk <adrian_frazier_kim@asl.org>
2025-01-25 17:40:32 +0000

    Jonny-Tan accept my pull req within 5 DAYS this time

--- src/main/java/frc/robot/subsystems/elevator/ElevatorConstants.java
+++ src/main/java/frc/robot/subsystems/elevator/ElevatorConstants.java
@@ -1,10 +1,9 @@
 package frc.robot.subsystems.elevator;
 
 public class ElevatorConstants {
-    public static final int ELEVATOR_ID = 1;
+  public static final int ELEVATOR_ID = 1;
 
-    public static final double kp = 1;
-    public static final double ki = 1;
-    public static final double kd = 1;
+  public static final double kp = 1;
+  public static final double ki = 1;
+  public static final double kd = 1;
 }
-
