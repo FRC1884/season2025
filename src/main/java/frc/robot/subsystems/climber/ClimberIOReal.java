52651defbd942e7aae6837a5e169c6c054165da0
masterswordio <68434292+masterswordio@users.noreply.github.com>
2025-01-31 16:13:30 +0000

    Implemented MaxMotion

--- src/main/java/frc/robot/subsystems/climber/ClimberIOReal.java
+++ src/main/java/frc/robot/subsystems/climber/ClimberIOReal.java
@@ -1,6 +1,8 @@
 package frc.robot.subsystems.climber;
 
 import com.revrobotics.RelativeEncoder;
+import com.revrobotics.spark.SparkBase;
+import com.revrobotics.spark.SparkClosedLoopController;
 import com.revrobotics.spark.SparkLowLevel;
 import com.revrobotics.spark.SparkMax;
 import edu.wpi.first.math.controller.PIDController;
@@ -9,12 +11,11 @@ public class ClimberIOReal implements ClimberIO {
 
   private SparkMax motor =
       new SparkMax(ClimberConstants.CLIMBER_ID, SparkLowLevel.MotorType.kBrushless);
-  private PIDController pid =
-      new PIDController(ClimberConstants.kp, ClimberConstants.ki, ClimberConstants.kd);
+  private SparkClosedLoopController controller = motor.getClosedLoopController();
   private RelativeEncoder encoder = motor.getEncoder();
 
   public void setPosition(double position) {
-    motor.setVoltage(pid.calculate(encoder.getPosition(), position));
+    controller.setReference(position, SparkBase.ControlType.kMAXMotionPositionControl);
   }
   ;
 
--- src/main/java/frc/robot/subsystems/pivot/PivotIOReal.java
+++ src/main/java/frc/robot/subsystems/pivot/PivotIOReal.java
@@ -1,6 +1,8 @@
 package frc.robot.subsystems.pivot;
 
 import com.revrobotics.RelativeEncoder;
+import com.revrobotics.spark.SparkBase;
+import com.revrobotics.spark.SparkClosedLoopController;
 import com.revrobotics.spark.SparkLowLevel;
 import com.revrobotics.spark.SparkMax;
 import edu.wpi.first.math.controller.PIDController;
@@ -9,12 +11,11 @@ public class PivotIOReal implements PivotIO {
 
   private SparkMax motor =
       new SparkMax(PivotConstants.PIVOT_ID, SparkLowLevel.MotorType.kBrushless);
-  private PIDController pid =
-      new PIDController(PivotConstants.kp, PivotConstants.ki, PivotConstants.kd);
+  private SparkClosedLoopController controller = motor.getClosedLoopController();
   private RelativeEncoder encoder = motor.getEncoder();
 
   public void setDegrees(double degrees) {
-    motor.setVoltage(pid.calculate(encoder.getPosition(), degrees));
+    controller.setReference(degrees, SparkBase.ControlType.kMAXMotionVelocityControl);
   }
   ;
 
